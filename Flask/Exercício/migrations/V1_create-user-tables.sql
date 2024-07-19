f3fccf91fc673ac8a6c7618edcd8193d3d13c201
Campos032 <marciocamposjunior744@gmail.com>
2024-07-19 16:40:57 -0300

    chore: update user migration

--- Flask/Exercício/migrations/V1_create-user-tables.sql
+++ Flask/Exercício/migrations/V1_create-user-tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     user_id VARCHAR(100) PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
     nickname VARCHAR(50) UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
