0813f10de11547b47bba4a713ac6f52fee29662e
Campos032 <marciocamposjunior744@gmail.com>
2024-07-19 16:49:31 -0300

    chore: update users entities

--- Flask/Exercício/app/models/entities/users.py
+++ Flask/Exercício/app/models/entities/users.py
@@ -2,9 +2,10 @@ from app.models.settings.base import Base
 from sqlalchemy import Column, String, TIMESTAMP
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+from flask_login import UserMixin
 
 
-class Users(Base):
+class Users(Base, UserMixin):
     __tablename__ = "users"
     
     user_id = Column(String, primary_key=True)
@@ -14,6 +15,9 @@ class Users(Base):
     created_at = Column(TIMESTAMP, default=func.current_timestamp())
 
     tasks = relationship("UserTaskAssociation", back_populates="user")
+    
+    def get_id(self):
+        return self.user_id
     
     def __repr__(self):
         return f"Users [nickname={self.nickname}, email={self.email}, user_id={self.user_id}"
